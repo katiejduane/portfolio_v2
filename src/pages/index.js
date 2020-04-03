388ac1dcfa52de858f54d16b13f888e54a62c5a5
Katie Duane <katiejduane@gmail.com>
2020-04-02 21:03:49 -0400

    link change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi there!</h1>
+    <h2>About</h2>
+    <p>Welcome to Katie Duane's new website</p>
+    <p>As your can see, there is nothing here yet. Soon, there will be!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/projects/">Go to page 2</Link>
   </Layout>
 )
 
