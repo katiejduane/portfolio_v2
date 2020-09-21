099da9ef066bb88b3d120773c7423f6dc748320d
Katie Duane <katiejduane@gmail.com>
2020-09-20 20:15:50 -0400

    major spelling issues i guess

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -6,8 +6,6 @@ import styles from "../styles/layout.module.scss"
 import jasper from "../assets/creative/Jasper.pdf"
 import writingImg from "../assets/images/journal.png"
 
-// TO DO: Add list component for edu/training in writing as well as published works...
-
 export default function WritingPage() {
   return (
     <Layout>
@@ -16,13 +14,13 @@ export default function WritingPage() {
         title="Writing & Art"
         image={writingImg}
         alt="Writing journal"
-        p1="I'm currently an MFA candidate in the low-residency program at the Rainier Writing Workshop, part of Pacific Luthern University.
+        p1="I'm currently an MFA candidate in the low-residency program at the Rainier Writing Workshop, part of Pacific Lutheran University.
         I tend to write nonfiction and my work often explores the connections between the personal and the scientific. 
-        I am especially fascinated by time, impermanance, biology, physics, and the natural world. Please find links to 
+        I am especially fascinated by time, impermanance, biology, geology, physics, and the natural world. Please find links to 
         published works below. Additional pieces forthcoming."
         p2="I also have degrees in Illustration and Art Education; and have worked professionally
         as an artist, designer, photographer, and teacher. At present, I find I'm happier working in technology, 
-        and living as an artist in my off hours :) I enjoy drawing, painting, collage and photgraphy. "
+        and living as an artist in my off hours :) I enjoy drawing, painting, collage and photography. "
       />
       <div className={styles.subContent}>
         <h4>Published Works</h4>
