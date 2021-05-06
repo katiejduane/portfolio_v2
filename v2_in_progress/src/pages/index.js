e1a4350c046d311adf64d1f2af5b80666bb0e030
Katie Duane <katiejduane@gmail.com>
2021-05-06 14:43:20 -0400

    teeny tiny updates

--- v2_in_progress/src/pages/index.js
+++ v2_in_progress/src/pages/index.js
@@ -16,16 +16,13 @@ const IndexPage = () => (
       title="Hello!"
       image={portrait}
       alt="Black and white photo of me"
-      p1="My name is Katie and I am a versatile, driven, and insatiably curious developer with a 
-      proclivity toward UI Development and Frontend Engineering. My background in arts and writing 
-      has prepared me with a keen eye, attention to detail, and innovative problem-solving skills. 
-      I love collaborating, communicating, and writing code that is clean, efficient, and accessible."
-      p2="Currently, I work as a Frontend Developer at Ware2Go out of Atlanta, GA. I love
-      opportunities to create or augment solutions to ensure that our code is as stable, efficient, 
-      clean, and DRY as possible."
-      p3="When I'm not writing code, it's very likely that I'm writing essays or poetry, painting, 
-      reading (usually 18 books at once), or spending time outside. I'm also a registered yoga teacher 
-      and love to travel."
+      p1="My name is Katie and I'm a web developer, writer, artist, and former high school art teacher.
+      I'm curious, driven, and love to collaborate. My background in the arts and writing has prepared 
+      me with a keen eye, attention to detail, and creative problem-solving skills."
+      p2="Currently, I work as a Frontend Developer at PLS 3rd Learning in Buffalo, NY. 
+      When I'm not writing code, it's very likely that I'm writing essays, painting, 
+      reading (usually 18 books at once), or spending time outside. I'm also a registered 
+      yoga teacher and love to travel."
     />
   </Layout>
 )
